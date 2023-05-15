f74e644e01d1a1af67bf2fbc3054e379057237c5
Pedro Figueiredo <pedro_sansone@estudante.sesisenai.org.br>
2023-05-15 15:45:30 -0300

    correção

--- cap02/js/ex2_1.js
+++ cap02/js/ex2_1.js
@@ -5,6 +5,6 @@ const resp = document.querySelector("h3")
 //cria um "ouvinte" de evento, acionado quando o botao submit for clicado 
 frm.addEventListener("submit", (e) => {
     const nome = frm.inNome.value            //obtem o nome digitado no form 
-    resp.innerText = "Olá ${nome}"           //exibe a resposta do programa 
+    resp.innerText=`Olá ${nome}`           //exibe a resposta do programa 
     e.preventDefault()                       //evita o envio do form 
 })
